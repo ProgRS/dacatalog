6f0bd37d1aaeb50d1d63c1fcf916c2efcbad8364
ProgRs <lfernandotc@hotmail.com>
2022-07-14 15:17:32 -0300

    Estilização do Navbar

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,16 @@
 import './assets/styles/custom.scss';
+import Navbar from 'components/Navbar';
 
 import './App.css';
 
-function App() {
-  return (
-    <h1 className="bg-primary">Hello DSCatalog</h1> 
+const App = ()  => {
+  return( 
+  
+    <>
+       <Navbar/> 
+      <h1>Hello DSCatalog</h1>
+    </>
+  
   );
 }
 
